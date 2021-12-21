6a361b57195b9661a3d7d16927ce12ff316aca68
lorenzo-guerrini <lorenzoguerrini8@live.it>
2021-12-21 01:13:01 +0100

    Aggiunto cambio di colore al click

--- script.js
+++ script.js
@@ -16,14 +16,26 @@ function start() {
 function gridGenerator(element) {
 
     for (let i = 0; i < 100; i++) {
+        //Crea gridSquare
         let gridSquare = document.createElement("div");
         gridSquare.classList.add("grid-square");
         gridSquare.innerHTML = '<div class="grid-number">1</div>';
-    
+
+        //Aggiunge eventListener al click e fa append all'elemento in input
+        gridSquare.addEventListener("click", addActiveClass);
         element.append(gridSquare);
     }
 
 }
+
+//Aggiunge la classe "active" ad un elemento
+function addActiveClass() {
+    this.classList.add("active");
+}
+
+//Funzione per cambiare colore al gridSquare quando cliccato
+
+
 
 // Funzione che gestisce le difficoltà
 // Easy? Genera questi elementi random. Hard? Genera questi altri
